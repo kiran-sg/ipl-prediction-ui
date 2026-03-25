6f5ab8de58765a9c61d7cae6e13e33b41618925c
kiran-sg <kiransg097@gmail.com>
2026-03-26 00:29:50 +0530

    proxy changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,26 @@
 import { ApplicationConfig, Injectable, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+ 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS, HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
 import { CommonService } from './common.service';
 import { CustomDatePipe } from './custom-date.pipe';
-
+ 
 @Injectable()
 class CredentialsInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    return next.handle(req.clone({ withCredentials: true }));
+    const userId = localStorage.getItem('userId');
+    const headers: {[name: string]: string} = {};
+    if (userId) {
+      headers['X-User-Id'] = userId;
+    }
+    return next.handle(req.clone({ withCredentials: true, setHeaders: headers }));
   }
 }
-
+ 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true },
